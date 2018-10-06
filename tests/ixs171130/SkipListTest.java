8b3daf6e1087e7058cc1eafb5df4c02f291a7aa0
sharayu <sharayumantri2554@gmail.com>
2018-10-05 23:30:26 -0500

    Added Rebuild and test case

--- tests/ixs171130/SkipListTest.java
+++ tests/ixs171130/SkipListTest.java
@@ -56,6 +56,17 @@ class SkipListTest {
         }
 
         assertEquals(Arrays.toString(expected), Arrays.toString(out));
+
+        s.rebuild();
+        it = s.iterator();
+        // make sure we have the correct order
+        for (int i = 0; i < 2000000; i++) {
+            out[i] = it.next();
+            expected[i] = i + 1;
+        }
+        System.out.println(Arrays.toString(out));
+//        s.printAllLevelOfCurrent();
+        assertEquals(Arrays.toString(expected), Arrays.toString(out));
     }
 
     @Test
@@ -88,22 +99,86 @@ class SkipListTest {
 
     private void iteratorIntegerRandom() {
         // build a skiplist with random order
-        SkipList<Integer> integerSkipList2 = new SkipList<>();
+        SkipList<Integer> integerSkipList = new SkipList<>();
         Integer[] toAdd = {1, 4, 2, 7, 5, 9, -10, 654, 34, 12, 86, -20, -999, 999, 100000, -1000000};
         Integer[] expected = {-1000000, -999, -20, -10, 1, 2, 4, 5, 7, 9, 12, 34, 86, 654, 999, 100000};
 
         for (int x : toAdd) {
-            integerSkipList2.add(x);
+            integerSkipList.add(x);
         }
 
         Integer[] res = new Integer[16];
-        Iterator<Integer> it = integerSkipList2.iterator();
+        Iterator<Integer> it = integerSkipList.iterator();
         int index = 0;
         while (it.hasNext()) {
             res[index] = it.next();
             index++;
         }
-
         assertEquals(Arrays.toString(expected), Arrays.toString(res));
+
+        integerSkipList.rebuild();
+        System.out.println(Arrays.toString(res));
+        integerSkipList.printAllLevelOfCurrent();
+
+        Integer[] toAdd1 = { 9, 1, 4, 2, 5, 6, 8, 3,7, 10, -1, 0, 13};
+        Integer[] expected1 = { -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13};
+        SkipList<Integer> integerSkipList1 = new SkipList<>();
+        for (int x : toAdd1) {
+            integerSkipList1.add(x);
+        }
+
+        Integer[] res1 = new Integer[13];
+        Iterator<Integer> it1 = integerSkipList1.iterator();
+        int index1 = 0;
+        while (it1.hasNext()) {
+            res1[index1] = it1.next();
+            index1++;
+        }
+        assertEquals(Arrays.toString(expected1), Arrays.toString(res1));
+
+        integerSkipList1.rebuild();
+        System.out.println(Arrays.toString(res1));
+        integerSkipList1.printAllLevelOfCurrent();
+
+
+        Integer[] toAdd2 = { 0 };
+        Integer[] expected2 = { 0 };
+        SkipList<Integer> integerSkipList2 = new SkipList<>();
+        for (int x : toAdd2) {
+            integerSkipList2.add(x);
+        }
+
+        Integer[] res2 = new Integer[1];
+        Iterator<Integer> it2 = integerSkipList2.iterator();
+        int index2 = 0;
+        while (it2.hasNext()) {
+            res2[index2] = it2.next();
+            index2++;
+        }
+        assertEquals(Arrays.toString(expected2), Arrays.toString(res2));
+
+        integerSkipList2.rebuild();
+        System.out.println(Arrays.toString(res2));
+        integerSkipList2.printAllLevelOfCurrent();
+
+        Integer[] toAdd3 = { 0, 1, 2, 3 };
+        Integer[] expected3 = { 0, 1, 2, 3 };
+        SkipList<Integer> integerSkipList3 = new SkipList<>();
+        for (int x : toAdd3) {
+            integerSkipList3.add(x);
+        }
+
+        Integer[] res3 = new Integer[4];
+        Iterator<Integer> it3 = integerSkipList3.iterator();
+        int index3 = 0;
+        while (it3.hasNext()) {
+            res3[index3] = it3.next();
+            index3++;
+        }
+        assertEquals(Arrays.toString(expected3), Arrays.toString(res3));
+
+        integerSkipList3.rebuild();
+        System.out.println(Arrays.toString(res3));
+        integerSkipList3.printAllLevelOfCurrent();
     }
 }
